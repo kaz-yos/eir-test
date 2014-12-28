b450f5a821d8666cb6253adb2943c7e728f19c3c
kaz-yos <kazky@mac.com>
2014-12-27 23:39:16 -0500

    Make Racket code more readable by []

--- eir-test.rkt
+++ eir-test.rkt
@@ -2,8 +2,8 @@
 
 (define (fib n)
   (cond
-   ((= n 0) 1)
-   ((= n 1) 1)
-   (else (+ (fib (- n 1)) (fib (- n 2))))))
+   [(= n 0) 1]
+   [(= n 1) 1]
+   [else (+ (fib (- n 1)) (fib (- n 2)))]))
 
 (map fib (range 10))
